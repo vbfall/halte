eb1aaede9ecf625227dfd0059ca99a739887e631
Vitor Falleiros <vbfall@gmail.com>
2019-01-21 12:03:03 -0500

    configure application.py

---
+++ webapp/application.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, render_template, request
+
+# Configure application
+app = Flask(__name__)
+
+# Following two blocks are for debugging:
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+# End of debugging blocks
+
+@app.route("/")
+def index():
+    """Show welcome message"""
+    return render_template("index.html")
