cf087723a229ab95c0df35dcfbcda6f90f84fd8b
Vitor Falleiros <vbfall@gmail.com>
2020-01-09 13:16:59 -0500

    vf improve structure

---
+++ src/data/data_pipeline.py
@@ -0,0 +1,8 @@
+
+
+class DataPipeline(object):
+
+    def __init__(self):
+        pass
+
+        
---
+++ src/models/weapon_classifier.py
@@ -0,0 +1,22 @@
+
+import tensorflow as tf
+from tensorflow import keras
+
+# from PIL import Image
+import numpy as np
+# import matplotlib.pyplot as plt
+import os
+import pathlib
+# from sklearn.model_selection import train_test_split
+
+
+class WeaponClassifier(object):
+
+    def __init__(self):
+        pass
+
+    def train(self, train_dataset, valid_dataset=None):
+        pass
+
+    def evaluate(self, test_dataset):
+        pass
