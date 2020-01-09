d9b74f9b56065115f7a9ee1abca76a4b9a136d4b
Vitor Falleiros <vbfall@gmail.com>
2020-01-09 15:29:13 -0500

    vf add basic working model, driver

---
+++ src/driver.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from data.data_pipeline import DataPipeline
+from models.weapon_classifier import WeaponClassifierModel
+# import foundations
+
+data_path = '../data/raw/images'
+BATCH_SIZE=32
+
+print('#### SET UP DATA PIPELINE ####')
+data_pipeline = DataPipeline(data_path)
+data_pipeline.split_data()
+
+train_dataset = data_pipeline.load_dataset()
+train_dataset = data_pipeline.prepare_for_training(train_dataset, batch_size=BATCH_SIZE)
+
+test_dataset = data_pipeline.load_dataset(train=False)
+test_dataset = data_pipeline.prepare_for_training(test_dataset, batch_size=BATCH_SIZE)
+
+STEPS_PER_EPOCH = int(np.ceil(data_pipeline.image_count/BATCH_SIZE))
+
+print('#### INITIALIZE AND TRAIN MODEL ####')
+weapon_classifier = WeaponClassifierModel(input_shape=(data_pipeline.IMG_WIDTH, data_pipeline.IMG_HEIGHT,3))
+weapon_classifier.train(train_dataset, STEPS_PER_EPOCH=STEPS_PER_EPOCH, epochs=3)
+
+print('#### EVALUATE MODEL ####')
+weapon_classifier.evaluate(test_dataset)
--- src/models/weapon_classifier.py
+++ src/models/weapon_classifier.py
@@ -2,21 +2,31 @@
 import tensorflow as tf
 from tensorflow import keras
 
-# from PIL import Image
-import numpy as np
-# import matplotlib.pyplot as plt
-import os
-import pathlib
-# from sklearn.model_selection import train_test_split
+
+class WeaponClassifierModel(object):
+
+    def __init__(self, input_shape):
+        print('Creating keras model...')
+        self.model = keras.Sequential([
+            keras.layers.Flatten(input_shape=input_shape),
+            keras.layers.Dense(96, activation='relu'),
+            keras.layers.Dense(4, activation='softmax')
+            ])
 
 
-class WeaponClassifier(object):
+    def train(self, train_dataset, STEPS_PER_EPOCH=2, epochs=3):
 
-    def __init__(self):
-        pass
+        # opt = RMSprop(lr=self.hyperparameters['learning_rate'], decay=self.hyperparameters['decay'])
 
-    def train(self, train_dataset, valid_dataset=None):
-        pass
+        print('Compiling model...')
+        self.model.compile(optimizer='adam',
+            loss='sparse_categorical_crossentropy',
+            metrics=['accuracy'])
+
+        print('Training model...')
+        self.model.fit(train_dataset, steps_per_epoch=STEPS_PER_EPOCH, epochs=epochs)
+
 
     def evaluate(self, test_dataset):
-        pass
+        test_loss, test_acc = self.model.evaluate(test_dataset, steps=1, verbose=2)
+        print('Accuracy on test dataset: {}'.format(test_acc))
